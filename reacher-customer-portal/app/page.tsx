15d7581160277309fa0e8ac1c4e1ba12ebe515d7
JerryYiyang <66449672+JerryYiyang@users.noreply.github.com>
2024-04-08 23:06:56 -0700

    dropdown added

--- reacher-customer-portal/app/page.tsx
+++ reacher-customer-portal/app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "./components/Sidebar";
+import Dropdown from "./components/Dropdown";
 
 const Dashboard: React.FC = () => {
   return (
@@ -8,12 +9,15 @@ const Dashboard: React.FC = () => {
       <Sidebar />
       {/* Main content */}
       <div className="flex-1 bg-gray-100">
+        {/* Analytics dashboard */}
         <h1 className="text-2xl font-bold mt-10 ml-10">Analytics Dashboard</h1>
+        {/* grey seperator line */}
         <div className="ml-10 flex justify-start items-center">
           <div className="border-b border-gray-400 flex-1 mt-10"></div>
         </div>
-        <div className="flex justify-center">
-          {/* Content for the main section */}
+        {/* Dropdown */}
+        <div className="ml-10 mt-10 flex justify-start items-center">
+          <Dropdown />
         </div>
       </div>
     </main>
